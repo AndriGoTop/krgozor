50213c4ab47a09aaffd351d338f1d9f296c33e9d
adndi <adndigolubenko@yandex.ru>
2023-12-04 23:01:21 +0300

    model

--- krugozor/offers/models.py
+++ krugozor/offers/models.py
@@ -11,16 +11,44 @@ class Offers(models.Model):
     picture = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Изображение')
     city = models.ForeignKey('City', on_delete=models.PROTECT, verbose_name='Город')
 
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Предложение'
+        verbose_name_plural = 'Предложения'
+
 
 class Feedback(models.Model):
     stars = models.ForeignKey('Stars', on_delete=models.PROTECT, verbose_name='Звезда')
     content = models.TextField(verbose_name='Контент')
     id_offer = models.CharField(max_length=150, verbose_name='ID предложения')
 
+    def __str__(self):
+        return self.id_offer
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+
 
 class City(models.Model):
     title = models.CharField(max_length=250, verbose_name='Название')
 
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Город'
+        verbose_name_plural = 'Города'
+
 
 class Stars(models.Model):
     title = models.CharField(max_length=1, verbose_name='Звезда')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Звезда'
+        verbose_name_plural = 'Звезды'
