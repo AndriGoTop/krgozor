88b3f34f1421360c700a3fd2e3068b9024a913fd
adndi <adndigolubenko@yandex.ru>
2023-12-06 18:39:59 +0300

    v 0.001

--- krugozor/offers/urls.py
+++ krugozor/offers/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, index_city, all_offers, content
 
 urlpatterns = [
-    path('', index)
+    path('', index, name='home'),
+    path('city/<int:city_id>/', index_city, name='city'),
+    path('all_offers/<int:city_id>/', all_offers, name='all_offers'),
+    path('content/<int:content_id>/', content, name='content'),
 ]
